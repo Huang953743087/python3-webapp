e4c7960a224ff10b51b3326a0da8a6e8e3b493d4
Zehao.Huang <953743087@qq.com>
2018-02-18 21:00:24 +0800

    add app ,orm.

--- www/app.py
+++ www/app.py
@@ -18,6 +18,55 @@ def init(loop):
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
+#创建连接池，连接池由全局变量__pool存储，缺省情况下将编码设置为utf8，自动提交事务
+@asyncio.coroutine
+def create_pool(loop,**kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield  from aiomysql(
+        host=kw.get('host','localhost'),
+        port=kw.get('port',3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset','utf8'),
+        autocommit=kw.get('autocommit',True),
+        maxsize=kw.get('maxsize',10),
+        minsize=kw.get('minsize',1),
+        loop=loop
+
+        )
+#SQL语句的占位符是?，而MySQL的占位符是%s，select()函数在内部自动替换。
+@asyncio.coroutine
+def select(sql,args,size=None):
+    log(sql,args)
+    global __pool
+    with(yield from __pool) as conn:
+        cur=yield from conn.cursor(aiomysql.DictCursor)
+        #用于替换？与%
+        yield from cur.execute(sql.replace('?','%s'),args or ())
+        if size:
+            rs=yield from curfetchmany(size)
+        else:
+            rs=yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned:%s'%len(rs))
+        return rs
+'''
+要执行INSERT、UPDATE、DELETE语句，可以定义一个通用的execute()函数，
+因为这3种SQL的执行都需要相同的参数，以及返回一个整数表示影响的行数：'''
+@asyncio.coroutine
+def excute(sql,args):
+    log(sql)
+    with(yield from __pool) as conn:
+        try:
+            cur=yield from conn.cursor()
+            yield from cur.execute(sql.replace('?','%s'),args)
+            affected=cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return  affected
 
 loop=asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
